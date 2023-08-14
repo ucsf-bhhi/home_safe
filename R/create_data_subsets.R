6799a1b1087d8fb61f9aecb0c7240b2906c32996
SaraMColomBrana <sara.colom@ucsf.edu>
2023-08-14 11:03:27 -0700

    add reporting agency to all data subsets

--- R/create_data_subsets.R
+++ R/create_data_subsets.R
@@ -1,13 +1,13 @@
 # Create data subsets
 
 demo_dat <- full_dat %>%
-  select(id, location_of_participation:other_income)
+  select(id, reporting_agency, location_of_participation:other_income)
 
 int_dat <- full_dat %>%
-  select(id, intervention_1_type:intervention_6_amount)
+  select(id, reporting_agency, intervention_1_type:intervention_6_amount)
 
 follow_dat <- full_dat %>%
-  select(id, six_month_follow_up_living_situation_verified_date:twelve_month_follow_up_substantiated_aps_reports)
+  select(id, reporting_agency, six_month_follow_up_living_situation_verified_date:twelve_month_follow_up_substantiated_aps_reports)
 
 case_dat <- full_dat %>%
   select(reporting_agency, id, case_start_date:client_referred_to_ces, case_closure_date:living_situation_at_exit)
